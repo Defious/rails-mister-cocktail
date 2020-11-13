ef6507679431ac7cb54ed90e5e031e8692f148f1
Bastien Duquesne <bastienmsn@gmail.com>
2020-11-13 16:46:41 +0100

    pic is done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.create(name: "Spritz")
-Cocktail.create(name: "Bloody Mary")
-Cocktail.create(name: "Daïquiri")
-Cocktail.create(name: "Cosmopolitan")
-Cocktail.create(name: "Marguarita")
-Cocktail.create(name: "Dry Martini")
-Cocktail.create(name: "Caipirinha")
-Cocktail.create(name: "Sex on the Beach")
-Cocktail.create(name: "White Russian")
-Cocktail.create(name: "Long Island Iced Tea")
+Cocktail.delete_all
